d881a2bfe02edad4145437efedebc63c24a77903
Joe Blau <im@joeblau.com>
2020-12-26 14:21:08 -0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Joe Blau on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
